2ed68282535ef5d370329966d39cca0534229c9c
KimCookieYa <min49590@gmail.com>
2024-08-01 01:55:49 +0900

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,17 @@
 import type { Config } from "tailwindcss";
 
+function generatePixelObject(max: number) {
+    return Array.from(Array(max)).reduce((acc, _, i) => {
+        acc[i] = `${i}px`;
+        return acc;
+    }, {}) as Record<string, string>;
+}
+
+const px0_10 = generatePixelObject(11);
+const px0_100 = generatePixelObject(101);
+const px0_200 = generatePixelObject(201);
+const px0_2000 = generatePixelObject(2001);
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,13 +19,26 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      extend: {
+          borderWidth: px0_10,
+          fontSize: px0_100,
+          lineHeight: px0_100,
+          minWidth: px0_2000,
+          minHeight: px0_2000,
+          maxWidth: px0_2000,
+          maxHeight: px0_2000,
+          spacing: px0_200,
+          borderRadius: px0_100,
+          width: px0_2000,
+          height: px0_2000,
+          inset: px0_2000,
       },
-    },
+      screens: {
+          'xl': { max: '1279px' },
+          'lg': { max: '1023px' },
+          'md': { max: '767px' },
+          'sm': { max: '576px' },
+      },
   },
   plugins: [],
 };
