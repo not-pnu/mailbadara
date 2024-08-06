27496daf0a81fc7f87d531c921f4da0e1ab4b8eb
KimCookieYa <min49590@gmail.com>
2024-08-07 00:25:36 +0900

    design: 랜딩페이지 UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ko">
-            <body className={'relative'}>{children}</body>
+            <body className={'relative h-screen w-screen'}>{children}</body>
         </html>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ function generatePixelObject(max: number) {
 const px0_10 = generatePixelObject(11);
 const px0_100 = generatePixelObject(101);
 const px0_200 = generatePixelObject(201);
+const px0_1000 = generatePixelObject(1001);
 const px0_2000 = generatePixelObject(2001);
 
 const config: Config = {
@@ -20,9 +21,9 @@ const config: Config = {
     ],
     theme: {
         extend: {
-            borderWidth: px0_10,
-            fontSize: px0_100,
-            lineHeight: px0_100,
+            borderWidth: px0_100,
+            fontSize: px0_1000,
+            lineHeight: px0_1000,
             minWidth: px0_2000,
             minHeight: px0_2000,
             maxWidth: px0_2000,
@@ -38,6 +39,12 @@ const config: Config = {
             lg: { max: '1023px' },
             md: { max: '767px' },
             sm: { max: '576px' },
+        },
+        fontFamily: {
+            // NanumMyeongjo: ['Nanum Myeongjo', 'serif'],
+            // NanumGothic: ['Nanum Gothic', 'sans-serif'],
+            NanumMyeongjo: ['Nanum Myeongjo'],
+            NanumGothic: ['Nanum Gothic'],
         },
     },
 };
