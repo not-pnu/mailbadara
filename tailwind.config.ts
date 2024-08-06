ae22662703f0429db713c92abcf24a7669bdd7be
KimCookieYa <min49590@gmail.com>
2024-08-07 00:06:49 +0900

    formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 function generatePixelObject(max: number) {
     return Array.from(Array(max)).reduce((acc, _, i) => {
@@ -13,33 +13,33 @@ const px0_200 = generatePixelObject(201);
 const px0_2000 = generatePixelObject(2001);
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-      extend: {
-          borderWidth: px0_10,
-          fontSize: px0_100,
-          lineHeight: px0_100,
-          minWidth: px0_2000,
-          minHeight: px0_2000,
-          maxWidth: px0_2000,
-          maxHeight: px0_2000,
-          spacing: px0_200,
-          borderRadius: px0_100,
-          width: px0_2000,
-          height: px0_2000,
-          inset: px0_2000,
-      },
-      screens: {
-          'xl': { max: '1279px' },
-          'lg': { max: '1023px' },
-          'md': { max: '767px' },
-          'sm': { max: '576px' },
-      },
-  },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            borderWidth: px0_10,
+            fontSize: px0_100,
+            lineHeight: px0_100,
+            minWidth: px0_2000,
+            minHeight: px0_2000,
+            maxWidth: px0_2000,
+            maxHeight: px0_2000,
+            spacing: px0_200,
+            borderRadius: px0_100,
+            width: px0_2000,
+            height: px0_2000,
+            inset: px0_2000,
+        },
+        screens: {
+            xl: { max: '1279px' },
+            lg: { max: '1023px' },
+            md: { max: '767px' },
+            sm: { max: '576px' },
+        },
+    },
 };
 
 export default config;
